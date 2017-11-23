19c865fcd410221b1493405a474ab327367e1755
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-23 14:32:17 +0100

    Add unit tests for Person class

---
+++ SerializePeopleTests/PersonTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerializePeople;
+using System.IO;
+
+namespace SerializePeopleTests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        Person testPerson = new Person("Lorea", Person.Genders.Female, new DateTime(1997, 3, 5));
+
+        [TestMethod]
+        public void Person_ConstructorWorksCorrectly()
+        {
+            Assert.AreEqual("Lorea", testPerson.Name);
+            Assert.AreEqual(Person.Genders.Female, testPerson.Gender);
+            Assert.AreEqual(new DateTime(1997, 3, 5), testPerson.BirthDate);
+        }
+
+        [TestMethod]
+        public void Person_ToString_ShowsCorrectly()
+        {
+            string expected = "Name: Lorea, Gender: Female, Date of Birth: 1997.03.05, Age: 20";
+            string actual = testPerson.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Person_Serialize_CreatesFile()
+        {
+            testPerson.Serialize("lorea.dat");
+            Assert.IsTrue(File.Exists("lorea.dat"));
+        }
+
+        [TestMethod]
+        public void Person_Serialization_WorksBothWay()
+        {
+            testPerson.Serialize("lorea.dat");
+            Person deserializedPerson = Person.Deserialize("lorea.dat");
+            Assert.AreEqual(testPerson.Name, deserializedPerson.Name);
+            Assert.AreEqual(testPerson.Gender, deserializedPerson.Gender);
+            Assert.AreEqual(testPerson.BirthDate, deserializedPerson.BirthDate);
+        }
+    }
+}
---
+++ SerializePeopleTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SerializePeopleTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SerializePeopleTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77b9d719-4776-46c8-b6d5-6c5b41d05dae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
