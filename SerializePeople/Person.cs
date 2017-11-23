611af93e69686873b69403d498ec6291ced852e1
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-23 11:29:25 +0100

    Add Person class

---
+++ SerializePeople/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SerializePeople
+{
+    [Serializable]
+    class Person
+    {
+        public enum Genders { Male, Female }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Genders Gender { get; set; }
+        public int Age => DateTime.Now.Year - BirthDate.Year;
+    }
+}
