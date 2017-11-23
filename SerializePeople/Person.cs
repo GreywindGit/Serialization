4f811998c4adfe0e780010be4b6ff7cde46c090c
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-23 12:09:14 +0100

    Add serializing method

--- SerializePeople/Person.cs
+++ SerializePeople/Person.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Reflection;
 
 namespace SerializePeople
 {
@@ -15,5 +18,29 @@ namespace SerializePeople
         public DateTime BirthDate { get; set; }
         public Genders Gender { get; set; }
         public int Age => DateTime.Now.Year - BirthDate.Year;
+
+        public void Serialize(string outputFile)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            PropertyInfo[] properties = typeof(Person).GetProperties();
+            Dictionary<string, object> personMap = new Dictionary<string, object>();
+            foreach (PropertyInfo property in properties)
+            {
+                personMap[property.Name] = property.GetValue(this);
+            }
+            if (File.Exists(outputFile))
+                File.Delete(outputFile);
+            using (FileStream stream = new FileStream(outputFile, FileMode.Create))
+            {
+                try
+                {
+                    formatter.Serialize(stream, personMap);
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine($"Failed to serialize due to: {e.Message}");
+                }
+            }
+        }
     }
 }
--- SerializePeople/Program.cs
+++ SerializePeople/Program.cs
@@ -10,6 +10,12 @@ namespace SerializePeople
     {
         static void Main(string[] args)
         {
+            Person Lorea = new Person();
+            Lorea.Name = "Lorea";
+            Lorea.BirthDate = new DateTime(1997, 3, 5);
+            Lorea.Gender = Person.Genders.Female;
+            Lorea.Serialize("lorea-data.dat");
+            Console.ReadKey();
         }
     }
 }
